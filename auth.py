15f32535e9c7d08beff177ec65b426f3ff94db4e
EvColls <evcollao08@gmail.com>
2025-10-07 11:07:08 +0800

    changed The Hashed Password

--- auth.py
+++ auth.py
@@ -51,7 +51,7 @@ fake_users_db = {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": hashed_password_for_johndoe,
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",  # secret
         "disabled": False,
     }
 }
